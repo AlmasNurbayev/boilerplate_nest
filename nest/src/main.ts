b6834678796bb9c899b70631aa868ff3b8fe39fb
almas <ntldr@mail.ru>
2024-03-21 14:37:28 +0500

    fix wrong port on starting

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -37,6 +37,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
