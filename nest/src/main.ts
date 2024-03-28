b84ab33051d42a4c13e5a376e6337f153fe0e1c4
almas <ntldr@mail.ru>
2024-03-28 15:51:38 +0500

    (main) - enable CORS

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -5,9 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { EntityNotFoundErrorFilter } from './filters/db.not_found.filter';
 import { QueryFailedFilter } from './filters/db.query_failed.filter';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.use(cookieParser());
 
   app.useGlobalFilters(new EntityNotFoundErrorFilter());
@@ -21,24 +24,19 @@ async function bootstrap() {
     }),
   );
   const options = new DocumentBuilder()
-    .setTitle('qtim API')
-    .setDescription('The API description')
-    .setVersion('1.0')
+    .setTitle(configService.get('swagger.title'))
+    .setDescription(configService.get('swagger.description'))
+    .setVersion(configService.get('swagger.version'))
     .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'JWT',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      'JWT-auth', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+      configService.get('swagger.bearer.options'),
+      configService.get('swagger.bearer.slug'),
     )
     .addCookieAuth('refresh_qtim')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors(configService.get('cors'));
 
   await app.listen(parseInt(process.env.APP_PORT) || 3000);
 }
